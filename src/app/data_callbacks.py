e5140529fc950c3f7c8ca97ef73ec5588351b7dc
jonas.schrage <119843859+joschrag@users.noreply.github.com>
2024-12-30 08:25:26 +0100

    [#4]: Change read_memory callbacks to write to sqlite db and add callbacks to read data from sqlite into app memory.

--- src/app/data_callbacks.py
+++ src/app/data_callbacks.py
@@ -1,13 +1,15 @@
 """This script contains the callbacks used in the apps data collection."""
 
 import logging
+import pathlib
 
 import numpy as np
 import pandas as pd
+import sqlalchemy as sa
 from dash import Input, Output, State, callback
 from dash.exceptions import PreventUpdate
 
-from src import PROCESS_NAME
+from src import PROCESS_NAME, engine
 from src.parser.building import Building
 from src.parser.lord import Lord
 from src.parser.read_data import read_config
@@ -37,13 +39,7 @@ def update_game_state(_: int) -> str:
     return sm.update_state(PROCESS_NAME)
 
 
-@callback(
-    Output("game_store", "data"),
-    Input("game_read", "n_intervals"),
-    State("game_store", "data"),
-    State("game_state", "data"),
-)
-def read_data_from_memory(n_intervals: int, data: list | None, state: str) -> list:
+def read_data_from_memory(n_intervals: int, game_state: str, pathname: str) -> None:
     """Read values from game memory.
 
     Args:
@@ -53,78 +49,24 @@ def read_data_from_memory(n_intervals: int, data: list | None, state: str) -> li
     Returns:
         list: game data
     """
-    data = data or []
-    game_data = pd.DataFrame(data)
-    if state == "game":
+    path_name = pathlib.Path(pathname)
+    game_uid = path_name.name
+    logger.info(game_uid)
+    if game_state == "game" and path_name.parent == pathlib.Path("/dashboard"):
         lord_glob_df = lord.get_lord_global_stats()
         lord_det_df = lord.get_lord_detailed_stats()
         buildings_df = b.calculate_all_stats()
         unit_df = unit.calculate_units()
-        cur_tick_df = (
-            pd.concat([lord_glob_df, lord_det_df], axis=1)
-            .merge(buildings_df, how="left", on="p_ID")
-            .merge(unit_df, how="left", on="p_ID")
-        )
-        cur_tick_df["time"] = n_intervals
-        game_data = pd.concat(
-            [game_data, cur_tick_df],
-            ignore_index=True,
-        )
-        return game_data.to_dict("records")
-    elif state == "lobby":
-        game_data = pd.DataFrame()
-    return game_data.to_dict("records")
+        cur_tick_df = pd.concat([lord_glob_df, lord_det_df], axis=1).merge(buildings_df, on=["p_ID"])
+        cur_tick_df["game_id"] = unit_df["game_id"] = game_uid
+        cur_tick_df["time"] = unit_df["time"] = n_intervals
+        with engine.begin() as conn:
+            cur_tick_df.to_sql("lords", conn, if_exists="append", index=False)
+            unit_df.to_sql("units", conn, if_exists="append", index_label="p_ID")
+        logger.info("Uploaded game data for tick %i", n_intervals)
 
 
-@callback(
-    Output("map_store", "data"),
-    Input("game_read", "n_intervals"),
-    State("map_store", "data"),
-    State("game_state", "data"),
-)
-def read_map_data_from_memory(n_intervals: int, data: list | None, state: str) -> list:
-    """Read values from game memory.
-
-    Args:
-        n_intervals (int): number of intervals passed
-        data (list | None): currently stored game data
-
-    Returns:
-        list: game data
-    """
-    data = data or []
-    map_data = pd.DataFrame(data)
-    if state == "game" and n_intervals is not None:
-        lord_glob_df = lord.get_map_settings()
-        lord_glob_df["end_month"] = lord_glob_df["end_month"] + 1
-        lord_glob_df["start_month"] = lord_glob_df["start_month"] + 1
-        lord_glob_df["year_month"] = (
-            lord_glob_df["end_year"].astype(str) + "-" + lord_glob_df["end_month"].astype(str)
-        ).astype("datetime64[s]")
-        if ((lord_glob_df["start_year"] == 0) | (lord_glob_df["end_year"] == 0)).any():
-            raise PreventUpdate()
-        if not map_data.empty and map_data[lord_glob_df.columns.to_list()].tail(1).reset_index(drop=True).equals(
-            lord_glob_df
-        ):
-            raise PreventUpdate()
-        lord_glob_df["time"] = n_intervals
-        map_data = pd.concat(
-            [map_data, lord_glob_df],
-            ignore_index=True,
-        )
-        return map_data.to_dict("records")
-    elif state == "lobby":
-        map_data = pd.DataFrame()
-    return map_data.to_dict("records")
-
-
-@callback(
-    Output("lord_store", "data"),
-    Input("game_read", "n_intervals"),
-    State("lord_store", "data"),
-    State("game_state", "data"),
-)
-def save_lord_names(n_intervals: int, data: list | None, state: str) -> list:
+def save_lord_names(n_intervals: int, game_state: str, pathname: str) -> None:
     """Store the lord names into app memory.
 
     Args:
@@ -137,18 +79,180 @@ def save_lord_names(n_intervals: int, data: list | None, state: str) -> list:
     Returns:
         list: lord names list
     """
-    if isinstance(data, list) and any(data):
-        old_df = pd.DataFrame(data)
-    else:
-        old_df = pd.DataFrame()
-    if state != "stats":
+    path_name = pathlib.Path(pathname)
+    game_uid = path_name.name
+    logger.info([game_uid, path_name.parent])
+    with engine.begin() as conn:
+        table = sa.Table("lord_names", sa.MetaData(), autoload_with=engine)
+        query = sa.Select(table).where(table.c.game_id == game_uid)
+        old_df = pd.read_sql(query, conn)
+    if n_intervals and game_state != "stats" and path_name.parent == pathlib.Path("/dashboard"):
         lord.get_active_lords()
         if lord.num_lords == 0:
             raise PreventUpdate()
         lord.get_lord_names()
         lord_df = pd.DataFrame(
-            {"p_ID": np.arange(1, lord.num_lords + 1), "lord_names": lord.lord_names, "teams": lord.teams}
+            {
+                "lord_names": lord.lord_names,
+                "p_ID": np.arange(1, lord.num_lords + 1),
+                "teams": lord.teams,
+                "game_id": [game_uid] * lord.num_lords,
+            }
         )
         if not lord_df.equals(old_df):
-            return lord_df.to_dict("records")
+            with engine.begin() as conn:
+                lord_df.to_sql("lord_names", conn, if_exists="append", index=False)
+
+
+@callback(
+    Input("game_read", "n_intervals"),
+    State("map_store", "data"),
+    State("game_state", "data"),
+    State("loc", "pathname"),
+)
+def read_map_data_from_memory(n_intervals: int, map_data: list | None, game_state: str, pathname: str) -> None:
+    """Read values from game memory.
+
+    Args:
+        n_intervals (int): number of intervals passed
+        data (list | None): currently stored game data
+
+    Returns:
+        list: game data
+    """
+    map_data = map_data or []
+    map_df = pd.DataFrame(map_data)
+    path_name = pathlib.Path(pathname)
+    game_uid = path_name.name
+    if game_state == "game" and n_intervals is not None and path_name.parent == pathlib.Path("/dashboard"):
+        read_data_from_memory(n_intervals, game_state, pathname)
+        save_lord_names(n_intervals, game_state, pathname)
+        lord_glob_df = lord.get_map_settings()
+        lord_glob_df["end_month"] = lord_glob_df["end_month"] + 1
+        lord_glob_df["start_month"] = lord_glob_df["start_month"] + 1
+        if not ((lord_glob_df["start_year"] == 0) | (lord_glob_df["end_year"] == 0)).any():
+            lord_glob_df["year_month"] = (
+                (lord_glob_df["end_year"].astype(str) + "-" + lord_glob_df["end_month"].astype(str))
+                .astype("datetime64[s]")
+                .dt.strftime("%Y-%m-%dT%H:%M:%S")
+            )
+
+            if map_df.empty or not map_df[lord_glob_df.columns.to_list()].tail(1).reset_index(drop=True).equals(
+                lord_glob_df
+            ):
+                lord_glob_df["time"] = n_intervals
+                lord_glob_df["game_id"] = game_uid
+                with engine.begin() as conn:
+                    lord_glob_df.to_sql("map_data", conn, if_exists="append", index=False)
+
+
+@callback(
+    Output("map_store", "data"),
+    Input("game_read", "n_intervals"),
+    State("loc", "pathname"),
+)
+def read_map_data_from_sql(n_intervals: int, pathname: str) -> list:
+    """Read the map_data table from sql and save data to store.
+
+    Args:
+        n_intervals (int): number of intervals
+        pathname (str): current app browser path
+
+    Raises:
+        PreventUpdate: No new data to update store
+
+    Returns:
+        list: map data
+    """
+    game_uid = pathlib.Path(pathname).name
+    if n_intervals and game_uid:
+        table = sa.Table("map_data", sa.MetaData(), autoload_with=engine)
+        query = sa.Select(table).where(table.c.game_id == game_uid)
+        with engine.begin() as conn:
+            dataframe = pd.read_sql(query, conn)
+        return dataframe.to_dict("records")
+    raise PreventUpdate()
+
+
+@callback(
+    Output("lord_store", "data"),
+    Input("game_read", "n_intervals"),
+    State("loc", "pathname"),
+)
+def read_lord_data_from_sql(n_intervals: int, pathname: str) -> list:
+    """Read the lord_data table from sql and save data to store.
+
+    Args:
+        n_intervals (int): number of intervals
+        pathname (str): current app browser path
+
+    Raises:
+        PreventUpdate: No new data to update store
+
+    Returns:
+        list: lord data
+    """
+    game_uid = pathlib.Path(pathname).name
+    if n_intervals and game_uid:
+        table = sa.Table("lords", sa.MetaData(), autoload_with=engine)
+        query = sa.Select(table).where(table.c.game_id == game_uid)
+        with engine.begin() as conn:
+            dataframe = pd.read_sql(query, conn)
+        return dataframe.to_dict("records")
+    raise PreventUpdate()
+
+
+@callback(
+    Output("unit_store", "data"),
+    Input("game_read", "n_intervals"),
+    State("loc", "pathname"),
+)
+def read_unit_data_from_sql(n_intervals: int, pathname: str) -> list:
+    """Read the unit_data table from sql and save data to store.
+
+    Args:
+        n_intervals (int): number of intervals
+        pathname (str): current app browser path
+
+    Raises:
+        PreventUpdate: No new data to update store
+
+    Returns:
+        list: unit data
+    """
+    game_uid = pathlib.Path(pathname).name
+    if n_intervals and game_uid:
+        table = sa.Table("units", sa.MetaData(), autoload_with=engine)
+        query = sa.Select(table).where(table.c.game_id == game_uid)
+        with engine.begin() as conn:
+            dataframe = pd.read_sql(query, conn)
+        return dataframe.to_dict("records")
+    raise PreventUpdate()
+
+
+@callback(
+    Output("lord_names", "data"),
+    Input("game_read", "n_intervals"),
+    State("loc", "pathname"),
+)
+def read_lord_names_data_from_sql(n_intervals: int, pathname: str) -> list:
+    """Read the lord_names_data table from sql and save data to store.
+
+    Args:
+        n_intervals (int): number of intervals
+        pathname (str): current app browser path
+
+    Raises:
+        PreventUpdate: No new data to update store
+
+    Returns:
+        list: lord names data
+    """
+    game_uid = pathlib.Path(pathname).name
+    if n_intervals and game_uid:
+        table = sa.Table("lord_names", sa.MetaData(), autoload_with=engine)
+        query = sa.Select(table).where(table.c.game_id == game_uid)
+        with engine.begin() as conn:
+            dataframe = pd.read_sql(query, conn)
+        return dataframe.to_dict("records")
     raise PreventUpdate()
